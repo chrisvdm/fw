8e54dd407eb4a8900156d1cd4c899604762806a3
chrisvdm <nymanchristine@gmail.com>
2021-04-22 15:45:50 +0200

    add basic react

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
       }
     ]
   },
+  devtool: 'inline-source-map',
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
